2213d9b7f76ea694dcffa6d17f0b556eeedaadd3
Vishal Singh <vishalsingh@Vishals-MacBook-Pro.local>
2023-05-24 15:59:45 +1000

    fix: Dummy fix

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn Github Actions
+          Learn Github Actions from scratch
         </a>
       </header>
     </div>
